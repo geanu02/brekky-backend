f8d5ff2d8262650e20e927b6197c64f33b2303cd
Gian Carlo Torres <geanu@outlook.com>
2023-06-04 23:02:44 -0400

    Remove json.dumps on api_content - doesn't work

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -23,8 +23,8 @@ def add_recipe(user):
             recipe_id = content['recipe_id'],
             recipe_title = content['recipe_title'],
             recipe_thumb = content['recipe_thumb'],
-            recipe_api_content = json.dumps(content['recipe_api_content']),
-            recipe_user_content = json.dumps(content['recipe_user_content']),
+            recipe_api_content = content['recipe_api_content'],
+            recipe_user_content = content['recipe_user_content'],
             recipe_api_url = content['recipe_api_url']
         )
         rec.commit()
