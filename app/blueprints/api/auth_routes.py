cc014d4e211f88f3c5565203d64d0a2c8d9e912d
Gian Carlo Torres <geanu@outlook.com>
2023-06-19 23:49:38 -0400

    Update Account

--- app/blueprints/api/auth_routes.py
+++ app/blueprints/api/auth_routes.py
@@ -83,31 +83,33 @@ def update_names(user):
     content = request.json
     first_name = content['first_name']
     last_name = content['last_name']
-
-    # Add Email and Username check for PUT method
-    
-    # user_check = User.query.filter_by(username=username).first()
-    # if user_check:
-    #     return jsonify([{
-    #         "message": "Username is taken. Try again.",
-    #         "success": False
-    #     }])
-    # email_check = User.query.filter_by(email=email).first()
-    # if email_check:
-    #     return jsonify([{
-    #         "message": "Email is already registered. Try again.",
-    #         "success": False
-    #     }])
+    username = content['username']
+    email = content['email']
+    user_check = User.query.filter_by(username=username).first()
+    if user_check:
+        return jsonify([{
+            "message": "Username is taken. Try again.",
+            "success": False
+        }])
+    email_check = User.query.filter_by(email=email).first()
+    if email_check:
+        return jsonify([{
+            "message": "Email is already registered. Try again.",
+            "success": False
+        }])
     if updateUser:
         updateUser.first_name = first_name
         updateUser.last_name = last_name
+        updateUser.email = email
+        updateUser.username = username
         updateUser.commit()
         return jsonify([{
-            "message": f"{user.username} successfully updated!",
+            "message": f"{updateUser.username} successfully updated!",
             "success": True,
-            "first_name": user.first_name,
-            "last_name": user.last_name,
-            "token": user.token
+            "first_name": updateUser.first_name,
+            "last_name": updateUser.last_name,
+            "username": updateUser.username,
+            "email": updateUser.email
         }])
     return jsonify([{
             "message": f"Update not successful.",
